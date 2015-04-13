458845b7dbcd17250bc73ed7ec1fa4aeb4d190e2
unknown <rob.sherman@PC110.evoenergy.local>
2015-04-13 16:58:23 +0100

    Update to local

--- app/main.js
+++ app/main.js
@@ -1,27 +1,43 @@
 angular.module('rafter', [
 
 ])
-    .controller('MainCtrl',  ['$scope', '$http', '$log', function ($scope, $http, $log) {
+    .service('RafterModel', function($http){
+      var model = this,
+          URLS = {
+            FETCH:'php/index.php'
+          },
+          reports;
 
-    /*get json data*/
-	$http.get('php/index.php').success(function(data) {
-		$scope.phpData = data;
-		console.log('php: ', $scope.phpData);
-	});
-    
-    $scope.surveyData = [
-        {"id":0,"project_id":"10-00012","project_name":"The School","score":3,"max":7, "project_manager": "Jo Smith","roof_team":"Bob Builder"},
-        {"id":1,"project_id":"06-02032","project_name":"The factory","score":7,"max":10, "project_manager": "Martin Steel","roof_team":"Bob Builder"},
-        {"id":2,"project_id":"81-00123","project_name":"House","score":4,"max":7, "project_manager": "Robbie","roof_team":"Job"},
-        {"id":3,"project_id":"23-01902","project_name":"House 2","score":9,"max":10, "project_manager": "Robbie","roof_team":"Awesome Installers"}
-    ];
-    
-    $scope.currentID = null;
-    
-    function setCurrentID(survey){
+          function extract(result){
+            return result.data;
+          }
+
+          function cacheReports(result) {
+            reports = extract(result);
+            return reports;
+          }
+
+          model.getReports = function() {
+            return $http.get(URLS.FETCH).then(cacheReports);
+          };
+
+    })
+
+    .controller('MainCtrl',  function ($scope, $log, RafterModel) {
+
+      var MainCtrl = this;
+
+      MainCtrl.reports = RafterModel.getReports();
+
+      function setCurrentID(survey){
         $scope.currentID = survey;
-    }
-    
-    $scope.setCurrentID = setCurrentID;
+        }
 
-}]);
+      RafterModel.getReports()
+        .then(function(result){
+        $scope.reports = result;
+      });
+      
+      $scope.currentID = null;
+      $scope.setCurrentID = setCurrentID;
+});
--- php/index.php
+++ php/index.php
@@ -16,7 +16,20 @@ if ($conn->connect_error) {
 $data = [];
 
 //customer info
-$query = mysqli_query($conn, "SELECT id, customer_name, project_id, customer_address, sector, project_manager, roofing_team, date_of_audit FROM saved_reports");
+$query = mysqli_query($conn, "SELECT
+r.id,
+r.evoenergy_id,
+r.customer_name,
+r.customer_address,
+r.date_of_audit,
+GROUP_CONCAT(CASE WHEN job_id = '1' THEN `people_name` ELSE NULL END
+             ORDER BY `project_id` ) AS project_manager,
+GROUP_CONCAT(CASE WHEN job_id = '2' THEN `people_name` ELSE NULL END
+             ORDER BY `project_id`) AS roofing_team
+FROM reports r
+LEFT JOIN installation ON r.id = installation.project_id
+LEFT JOIN people ON installation.people_id = people.people_id
+GROUP BY `id`;");
 while($r = mysqli_fetch_assoc($query)) {
     $data[] = $r;
 };
